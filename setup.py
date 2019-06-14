36bfd322de7785e6409833ac4713ccf4427b3e94
Guido Diepen <site-github@guidodiepen.nl>
2019-06-14 14:04:06 +0200

    Added classifiers to the setup.py file

--- setup.py
+++ setup.py
@@ -19,5 +19,16 @@ setup(
     long_description=open('README.md').read(),
     entry_points={'console_scripts' : [
             'livy_submit = livysubmit.__main__:main'
-        ]}
+        ]},
+    classifiers         = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
